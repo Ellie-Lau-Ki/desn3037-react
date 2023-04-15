cd7920d1f6ea1aef0214501fd6190cfff72c6dcf
Ellie Lau Ki <ki.lau@georgebrown.ca>
2023-04-14 21:29:49 -0400

    Populating individual pages and calibrating type

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,9 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container, Box } from '@mui/system';
+
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
@@ -5,22 +11,28 @@ import Clients from "./components/Clients";
 import Contact from "./components/Contact";
 import Menu from "./components/Menu";
 
+
 function App() {
   return (
     <div className="App">
 
-      <BrowserRouter>
-        <Menu />
-        <Routes>
+      <Container maxWidth="sm" sx={{
+        mt: 5
+      }}>
 
+        <BrowserRouter>
+          <Box sx={{ mb: 3 }}>
+            <Menu />
+          </Box>
+          <Routes>
 
+            <Route exact path="about" element={<About />} />
+            <Route exact path="clients" element={<Clients />} />
+            <Route exact path="contact" element={<Contact />} />
 
-          <Route exact path="about" element={<About />} />
-          <Route exact path="clients" element={<Clients />} />
-          <Route exact path="contact" element={<Contact />} />
-
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
